501b023cbfc188815337d8dcac89b7a8140fbaeb
BigJohnBos <boston.ballash@ucas-edu.net>
2025-11-21 14:35:33 -0700

    ogkhbk

--- python.py
+++ python.py
@@ -1,1 +1,11 @@
-print("hello")
+password= input("Password?")
+bob = 1
+while True:
+    if password==12345:
+        print("correct")
+        break
+    else:
+        print("incorrect")
+        bob += 1
+
+        
